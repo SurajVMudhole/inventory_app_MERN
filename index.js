1b464261afb414edf9ac3496dacf495b8b890b19
surajmudhole <svmdvg800@gmail.com>
2024-01-20 15:47:15 +0530

    added ejs and made layout

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
 import express from "express";
-const port = 3100;
-const server = express();
+import path from "path";
+import ejsLayouts from "express-ejs-layouts";
 import ProductController from "./src/controllers/product.controller.js";
+const server = express();
+const port = 3100;
+
+//set view engine
+server.set("view engine", "ejs");
+server.set("views", path.join(path.resolve(), "src", "views"));
+
 // create instance of productcontroller
 const productController = new ProductController();
-
-server.get("/", productController.getProducts);
+server.use(ejsLayouts);
 server.use(express.static("src/views"));
+server.get("/", productController.getProducts);
 
 server.listen(port, (err) => {
   if (err) console.log("Error listening to server: " + err);
-  else console.log(`SERVER listening on port ${port}`);
+  else console.log(`SERVER listening on port ${port} `);
 });
--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -4,9 +4,6 @@ import productModel from "../models/products.model.js";
 export default class ProductController {
   getProducts(req, res) {
     let products = productModel.get();
-    console.log(products);
-    return res.sendFile(
-      path.join(path.resolve(), "src", "views", "products.html")
-    );
+    res.render("products", { products: products });
   }
 }
